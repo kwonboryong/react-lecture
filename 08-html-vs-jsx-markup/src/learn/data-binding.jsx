0a0f3e72f79f2642d43666fd68d2c29cb40c55d1
kwonboryong <kwonboryong@naver.com>
2024-07-30 10:24:51 +0900

    update: 데이터 랜덤 바인딩

--- 08-html-vs-jsx-markup/src/learn/data-binding.jsx
+++ 08-html-vs-jsx-markup/src/learn/data-binding.jsx
@@ -1,8 +1,11 @@
-function DataBinding() {
-  // <div> 남용
-  // 요소를 선택하는 최후의 수단(기준)
-  // 1. HTML에는 이미 의미적인 요소가 준비되어 있음
-  // 2. 의미에 맞는 요소가 존재하지 않을 때 그 때!!! 비로서 <div> 사용
+import { randomNumber } from '../utils';
+
+function DataBinding({ statusMessages }) {
+  // [미션] 랜덤 로직을 작성해서 임의의 상태 메시지가 표시되도록 설정합니다.
+  // Math.random() / Math.floor() / Math.round()
+
+  const statusMessage =
+    statusMessages[randomNumber(0, statusMessages.length - 1)];
 
   return (
     <>
@@ -11,13 +14,11 @@ function DataBinding() {
         <p>상태 메시지(status message)를 연결해 화면에 출력합니다.</p>
         <span className="status">
           {/* statusMessage 값을 화면에 표시합니다. (랜덤 표시도 도전!) */}
+          {statusMessage}
         </span>
       </dd>
     </>
   );
 }
-
-// JSX runtime (classic): React.createElement(React.Fragment, props, children)
-// JSX runtime (automatic): jsx(React.Fragment, props)
 
 export default DataBinding;
