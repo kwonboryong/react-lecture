e75a50bfd29cc43cd80dbf3a9ca41bf860350d45
kwonboryong <kwonboryong@naver.com>
2024-08-02 17:07:03 +0900

    update: 08/02 진도

--- 09-component-purity/src/pages/AvatarListPage.jsx
+++ 09-component-purity/src/pages/AvatarListPage.jsx
@@ -1,38 +1,27 @@
 // --------------------------------------------------------------------------
 // ✅ 컴포넌트 로직 재구성
 // --------------------------------------------------------------------------
-// - [ ] 데이터 분리
-// - [ ] 리스트 렌더링
+// - [x] 데이터 분리
+// - [x] 리스트 렌더링
+// - [x] 컴포넌트 속성 검사
 // - [ ] 컴포넌트 순수성 진단
 // --------------------------------------------------------------------------
-
+import { arrayOf } from 'prop-types';
 import Avatar from '@/components/Avatar';
+import { UserType } from '@/@types/globals.d';
 
+AvatarListPage.propTypes = {
+  list: arrayOf(UserType).isRequired,
+};
 
-const data = [
-  {name: "야무", photo:"man-02.jpg", status:"online"},
-  {name: "범쌤", photo:"man-02.jpg", status:"online"},
-  {name: "주원", photo:"man-02.jpg", status:"online"},
-  {name: "정민", photo:"man-02.jpg", status:"online"},
-]
-
-
-
-function AvatarListPage() {
+function AvatarListPage({ list }) {
   return (
     <ul className="AvatarList">
-      <li>
-        <Avatar name="야무" photo="man-02.jpg" status="online" />
-      </li>
-      <li>
-        <Avatar name="범쌤" photo="man-04.jpg" status="away" />
-      </li>
-      <li>
-        <Avatar name="주원" photo="woman-04.jpg" status="dont-disturb" />
-      </li>
-      <li>
-        <Avatar name="정민" photo="woman-01.jpg" />
-      </li>
+      {list.map((user) => (
+        <li key={user.id}>
+          <Avatar user={user} />
+        </li>
+      ))}
     </ul>
   );
 }
