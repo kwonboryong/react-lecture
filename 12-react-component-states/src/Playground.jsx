881cf613388d457c0025544a0b7c4271df817cbc
kwonboryong <kwonboryong@naver.com>
2024-08-05 19:39:34 +0900

    update: 08/05 진도

---
+++ 12-react-component-states/src/Playground.jsx
@@ -0,0 +1,11 @@
+import Counter from '@/components/Counter';
+
+function Playground() {
+  return (
+    <>
+      <Counter count={10} step={4} min={20} max={40} />
+    </>
+  );
+}
+
+export default Playground;
