2da7c4ece4530f8c723cf7c76910f69b6e8dac20
kwonboryong <kwonboryong@naver.com>
2024-08-05 09:22:48 +0900

    update: 08/05 환경 설정

--- 10-responding-to-events/src/learn/responding-to-events/event-propagation.jsx
+++ 10-responding-to-events/src/learn/responding-to-events/event-propagation.jsx
@@ -4,25 +4,20 @@
 // - [x] 전파 중지 (개별적으로 할 수 있음)
 // - [x] 전파 대안으로 핸들러 전달
 // - [x] 기본 작동 방지
-// --------------------------------------------------------------------------
-
-import LayoutBox from './LayoutBox';
-
-// RGB 모드
-// CMYK 모드
-
-// event.preventDefault(); // 이벤트 기본 작동을 차단
 
 // Event Delegation (위임)
 // Event Propagation (전파)
 // event.stopPropagation(); // 이벤트 전파 중지
+// event.preventDefault(); // 이벤트 기본 작동을 차단
+// --------------------------------------------------------------------------
+
+import LayoutBox from './LayoutBox';
+
 
 function EventPropagation() {
-  // const returnHandlePrint = (color) => (e) => {
-  //   console.log(color, e.target);
-  // };
 
   function returnHandlePrint(color) {
+
     return function handlePrint(e) {
       console.log(color, e.target);
     };
@@ -33,7 +28,7 @@ function EventPropagation() {
       <summary>
         <b>이벤트 전파 &amp; 기본 작동 방지</b>
       </summary>
-      {/* 상위 컴포넌트 : 정민 */}
+
       <LayoutBox style={styles.cyan} onClick={returnHandlePrint('cyan')}>
         <LayoutBox
           style={styles.magenta}
